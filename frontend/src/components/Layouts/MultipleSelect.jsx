f5fe4cb8d8eb008c15cf34ef87c411725903c256
MalekHamdeh <malek_h98@outlook.com>
2023-05-13 16:30:53 +0300

    fixed colors for feed filter select

--- frontend/src/components/Layouts/MultipleSelect.jsx
+++ frontend/src/components/Layouts/MultipleSelect.jsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import ListItemText from '@mui/material/ListItemText';
-import Select from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
-import './MultipleSelect.css';
-import { shouldReRender, tagsFiltering } from '../../actions/postAction';
-import { useState, useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox, { checkboxClasses } from "@mui/material/Checkbox";
+import "./MultipleSelect.css";
+import { shouldReRender, tagsFiltering } from "../../actions/postAction";
+import { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -73,8 +73,13 @@ export default function MultipleSelectCheckmarks({ array }) {
 
   return (
     <div>
-      <FormControl variant='standard' sx={{ m: 1, width: '94%' }}>
-        <InputLabel id='demo-multiple-checkbox-label'>Tag</InputLabel>
+      <FormControl variant='standard' sx={{ m: 1, width: "94%" }}>
+        <InputLabel
+          id='demo-multiple-checkbox-label'
+          sx={{ fontWeight: "500" }}
+        >
+          Filter
+        </InputLabel>
         <Select
           labelId='demo-multiple-checkbox-label'
           id='demo-multiple-checkbox'
@@ -92,15 +97,29 @@ export default function MultipleSelectCheckmarks({ array }) {
             }
           }}
           renderValue={(selected) =>
-            selected.map((element) => element.name).join(',')
+            selected.map((element) => element.name).join(",")
           }
           MenuProps={MenuProps}
           variant='standard'
+          sx={{
+            ":before": { borderBottomColor: "transparent" },
+            ":after": {
+              borderBottomColor: "#5B064A",
+            },
+            "&:hover:not(.Mui-disabled):before": {
+              borderBottomColor: "transparent",
+            },
+          }}
         >
           {array.map(({ name, _id }) => {
             return (
               <MenuItem key={_id} value={{ name, id: _id }}>
                 <Checkbox
+                  sx={{
+                    [`&, &.${checkboxClasses.checked}`]: {
+                      color: "#5B064A",
+                    },
+                  }}
                   checked={
                     checkboxLabel.findIndex(
                       (element) => element.name === name
