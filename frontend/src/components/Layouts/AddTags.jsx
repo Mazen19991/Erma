91a5b34404e45d16da8145f7b109402e01b3fcea
Mazen <mazen.abohassan@hotmail.com>
2023-05-18 20:59:29 +0300

    New post tags frontend is approximately finished

--- frontend/src/components/Layouts/AddTags.jsx
+++ frontend/src/components/Layouts/AddTags.jsx
@@ -1,11 +1,59 @@
 import React from "react";
-import { Chip } from "@mui/material";
+import { Chip, colors } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { TAGS_FILTERING } from "../../constants/postConstants";
 
 const AddTags = () => {
+  const [tagsArray, setTagsArray] = useState([]);
+  const [chipsSelected, setChipsSelected] = useState([]);
+
+  const { tags } = useSelector((state) => state.postOfFollowing);
+
+  useEffect(() => {
+    if (tags !== undefined) {
+      console.log("tags", tags);
+      setTagsArray(tags);
+    }
+  }, [tags]);
+
+  const chipsHandler = (tag) => {
+    if (!chipsSelected.includes(tag)) setChipsSelected([...chipsSelected, tag]);
+    console.log(chipsSelected);
+  };
+  const removeChipsHandler = (tag) => {
+    if (chipsSelected.includes(tag)) {
+      const index = chipsSelected.findIndex((element) => element === tag);
+      console.log(index);
+      if (index > -1) {
+        chipsSelected.splice(index, 1);
+        setChipsSelected([...chipsSelected]);
+      }
+      console.log(chipsSelected);
+    }
+  };
+
   return (
-    <div className='flex  gap-3 justify-between'>
-      <Chip label='Add Tags' onClick={() => {}} onDelete={() => {}} />
-      <Chip label='Add Tags' onClick={() => {}} onDelete={() => {}} />
+    <div className="flex  gap-3 justify-between">
+      {tagsArray.map(({ name, _id }) => {
+        return (
+          <>
+            <Chip
+              label={name}
+              key={_id}
+              onClick={() => {
+                chipsHandler(name);
+              }}
+              onDelete={() => {
+                removeChipsHandler(name);
+              }}
+            />
+          </>
+        );
+      })}
+      {
+        chipsSelected
+      }
     </div>
   );
 };
